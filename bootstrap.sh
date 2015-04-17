07aefcbbede67af2c67016dba7421221a9f30c4c
elgor <emanuel.regnath@tum.de>
2015-04-17 18:16:50 +0200

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,4 +15,13 @@ find . -type d -exec mkdir --parent $HOME_DIR/{} \;
 # create links
 find . -type f -exec ln -f -s $(pwd)/{} $HOME_DIR/{} \;
 
+read -p "Would you like to sync git? (y|n)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	git add *
+	git commit -a
+	git push origin master
+fi
+
 exit 0
